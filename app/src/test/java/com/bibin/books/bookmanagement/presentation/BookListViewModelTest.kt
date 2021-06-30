8dd3c4077a8bd50abb042756648ae89bb4e367f3
bibin <bibinpjacob91@gmail.com>
2021-06-30 12:41:33 +0100

    corrected unit testcase

--- app/src/test/java/com/bibin/books/bookmanagement/presentation/BookListViewModelTest.kt
+++ app/src/test/java/com/bibin/books/bookmanagement/presentation/BookListViewModelTest.kt
@@ -1,12 +1,10 @@
 package com.bibin.books.bookmanagement.presentation
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.MutableLiveData
 import com.bibin.books.base.domain.entity.ResponseEntity
 import com.bibin.books.bookmanagement.domain.BookListUseCase
 import com.bibin.books.bookmanagement.domain.entity.BookListEntity
 import com.bibin.books.bookmanagement.domain.entity.BooksEntity
-import com.bibin.books.bookmanagement.presentation.entity.Book
 import io.reactivex.Single
 import junit.framework.TestCase
 import org.junit.Assert
@@ -14,9 +12,9 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
-import javax.inject.Inject
 
 @RunWith(MockitoJUnitRunner::class)
 class BookListViewModelTest : TestCase() {
@@ -24,7 +22,7 @@ class BookListViewModelTest : TestCase() {
     @JvmField
     val instantExecutorRule = InstantTaskExecutorRule()
 
-    @Inject
+    @Mock
     lateinit var bookListUseCase: BookListUseCase
 
     private lateinit var viewModel: BookListViewModel
@@ -36,6 +34,7 @@ class BookListViewModelTest : TestCase() {
 
     @Test
     fun ` verify BookList size is 1 when GetBestSellerBooks`() {
+
         //given
         val category = "book"
         val responseEntityMock =
